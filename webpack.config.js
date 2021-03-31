cbcda0c1494bf32af790eca383cd4b266d4aab23
sk <seonk94@naver.com>
2021-03-31 20:39:47 +0900

    feat: sass loader 적용 및 css -> scss 수정

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,24 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-    mode: "none",
-    entry: "./example/index.js",
-    output: {
-        filename: "[name].js",
-        path: path.resolve("./dist"),
-    },
-    module: {
-        rules: [
-            {test: /\.css$/i, use: ['style-loader', 'css-loader'] }
-        ]
-    },
-    devServer: {
-        contentBase: path.join(__dirname, "dist"),
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "./example/index.html"
-        })
+  mode: 'none',
+  entry: './example/index.js',
+  output: {
+    filename: '[name].js',
+    path: path.resolve('./dist')
+  },
+  module: {
+    rules: [
+      { test: /\.(sa|sc|c)ss$/i, use: ['style-loader', 'css-loader', 'sass-loader'] }
     ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist')
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './example/index.html'
+    })
+  ]
 }
