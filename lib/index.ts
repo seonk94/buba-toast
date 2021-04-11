1e43881981d6c1f50f07a2ede4897d95aba5d7a3
sk <seonk94@naver.com>
2021-04-11 11:27:03 +0900

    feat: icon 수정, animation 추가

--- lib/index.ts
+++ lib/index.ts
@@ -1,5 +1,6 @@
 import "./index.scss";
 export type ToastType = "success" | "warn" | "info" | "danger";
+
 interface Option {
   icon: "success" | "warn" | "info" | "danger"
 }
@@ -18,49 +19,32 @@ function initContainer () {
   return toastContainer;
 }
 
+function createRow () {
+  const notificationElement = document.createElement('div');
+  notificationElement.id = 'notification';
+
+  return notificationElement;
+}
+
+function hideElement (element: HTMLElement) {
+  let opactiy = 1;
+  const animation = setInterval(() => {
+    opactiy -= 0.05;
+    element.style.opacity = String(opactiy);
+
+    if (opactiy <= 0) {
+      element.remove();
+      clearInterval(animation);
+    }
+  }, 10);
+}
+
 const toast = (message: string, type : ToastType = "success", option?: Option) => {
-
-  function createRow () {
-    const notificationElement = document.createElement('div');
-    notificationElement.id = 'notification';
-
-    return notificationElement;
-  }
-
-  function showElement (element: HTMLElement) {
-    element.style.opacity = '0';
-    let opacity = 0.05;
-    const animation = setInterval(() => {
-      opacity += 0.05;
-      element.style.opacity = String(opacity);
-
-      if (opacity >= 1) {
-        element.style.opacity = '1';
-        clearInterval(animation);
-      }
-    }, 10);
-
-    setTimeout(() => {
-      hideElement(element);
-    }, 3000);
-  }
-
-  function hideElement (element: HTMLElement) {
-    let opactiy = 1;
-    const animation = setInterval(() => {
-      opactiy -= 0.05;
-      element.style.opacity = String(opactiy);
-
-      if (opactiy <= 0) {
-        element.remove();
-        clearInterval(animation);
-      }
-    }, 10);
-  }
+  const containerElement = initContainer();
 
   const rowElement = createRow();
-  const containerElement = initContainer();
-  containerElement.append(rowElement);
+  
+  containerElement.prepend(rowElement);
   rowElement.innerHTML = `${message}`;
   rowElement.classList.add(type);
 
@@ -70,7 +54,10 @@ const toast = (message: string, type : ToastType = "success", option?: Option) =
     rowElement.prepend(iconElement);
   }
 
-  showElement(rowElement);
+  setTimeout(() => {
+    hideElement(rowElement);
+  }, 3000);
+
 
   return rowElement;
 };
